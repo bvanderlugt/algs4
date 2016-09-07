a7027f9055b25399e2ae60f4d5fd6ab14ebb3474
Blair VanderLugt <blair.vanderlugt@gmail.com>
2016-09-07 08:25:01 -0700

    adding to percolate

--- Percolation/src/Percolation.java
+++ Percolation/src/Percolation.java
@@ -1,10 +1,19 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 /**
- * Created by blair on 9/4/16.
+ * Created by Blair Vanderlugt on 9/4/16.
+ * Program to solve the percolation threshold using Monte Carlo simulation
+ * Run the program by redirecting standard input from a file
+ * java Percolation input10.txt
  */
+
 
 public class Percolation {
 
     private int[][] grid;
+    private int N;
 
     /**
      * create n-by-n grid, with all sites blocked
@@ -13,8 +22,9 @@ public class Percolation {
      */
     public Percolation(int n) throws IllegalArgumentException {
         if (n <= 0) {
-            throw new IllegalArgumentException("n must be greater than zero");
+            throw new IllegalArgumentException("input must be greater than zero");
         }
+        N = n;
         grid = new int[n][n];
     }
 
@@ -24,7 +34,8 @@ public class Percolation {
      * @param j
      */
     public void open(int i, int j) {
-       grid[i][j] = 1;
+        grid[i][j] = 1;
+
     }
 
     /**
@@ -35,6 +46,26 @@ public class Percolation {
      */
     public boolean isOpen(int i, int j) {
         return grid[i][j] == 1;
+    }
+
+    /**
+     * validate that input is within bounds of N
+     * @param p
+     */
+    private void validate(int p) {
+        if (p < 0 || p >= N) {
+            throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (N-1));
+        }
+    }
+
+    /**
+     * convert a point in a 2d grid to an index of a 1d array
+     * @param p
+     * @param q
+     * @return
+     */
+    private int xyTo1D(int p, int q) {
+        return (N * p) + q;
     }
 
     /**
--- Percolation/src/Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by blair on 9/4/16.
- */
-public class Test {
-    public static void main(String[] args) {
-        int[][] anArray = new int[10][10];
-        System.out.println(anArray[0][0]);
-    }
-}
